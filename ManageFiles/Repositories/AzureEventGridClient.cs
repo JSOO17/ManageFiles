d97d128c65aaae22620f9a2a01b12d8b2e027ace
Jaider Osorio <jaiderosorio87@gmail.com>
2024-06-03 12:57:17 -0500

    Agregar logging al envio de evento a event grid

--- ManageFiles/Repositories/AzureEventGridClient.cs
+++ ManageFiles/Repositories/AzureEventGridClient.cs
@@ -11,35 +11,42 @@ namespace ManageFiles.Repositories
         private const string TopicKey = "bz6FYYaDwNryTE1xXYGqsnICfv2l0heEgAZEGKYOtD4=";
 
         private EventGridPublisherClient _client;
+        private readonly ILogger<AzureEventGridClient> _logger;
 
-        public AzureEventGridClient()
+        public AzureEventGridClient(ILogger<AzureEventGridClient> logger)
         {
             var credentials = new AzureKeyCredential(TopicKey);
             _client = new EventGridPublisherClient(new Uri(TopicEndpoint), credentials);
+
+            _logger = logger;
         }
 
         public async Task SendEvent(int workItemId, FileModel file)
         {
             try
             {
+                var data = new
+                {
+                    WorkItemId = workItemId,
+                    File = file
+                };
+
                 var events = new List<EventGridEvent>
                 {
                     new EventGridEvent(
                         subject: "/ManageFiles/FileCreated",
                         eventType: "FileCreated",
                         dataVersion: "1.0",
-                        data: new
-                        {
-                            WorkItemId = workItemId,
-                            File = file
-                    })
+                        data: data)
                 };
 
                 await _client.SendEventsAsync(events);
+
+                _logger.LogInformation($"event sent. data: {data}");
             }
             catch (Exception ex)
             {
-
+                _logger.LogError("Something went wrong. ", ex);
             }
         }
     }
